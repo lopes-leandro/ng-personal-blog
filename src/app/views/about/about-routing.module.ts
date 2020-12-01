06ffd8abe5fff3563cfd2aa2800270e19a3060d9
Leandro Lopes <lopesleandro.it@gmail.com>
2020-12-01 20:53:35 -0300

    add routing app

--- src/app/views/about/about-routing.module.ts
+++ src/app/views/about/about-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: AboutComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
